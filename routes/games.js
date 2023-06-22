4557070dba7c8dffa5ac1eef56faee2218beb09d
Brendan96 <Brendan.ballard96@yahoo.com>
2023-06-22 08:44:55 -0500

    start code to upload pictures

--- routes/games.js
+++ routes/games.js
@@ -1,6 +1,9 @@
 import { Router } from 'express'
+import multer from 'multer'
 import * as gamesCtrl from '../controllers/games.js'
 import { isLoggedIn } from '../middleware/middleware.js'
+
+const upload = multer({ dest: 'uploads/' });
 
 const router = Router()
 
@@ -13,6 +16,14 @@ router.get('/:gameId', isLoggedIn, gamesCtrl.show)
 router.get('/:gameId/edit', isLoggedIn, gamesCtrl.edit)
 
 router.post('/', isLoggedIn, gamesCtrl.create)
+
+
+// NEW CODE BELOW FOR PICTURE UPLOADS
+
+router.post('/upload', upload.single('image'), (req,res) => {
+  console.log(req.file)
+  res.send('File uploaded!')
+})
 
 router.post('/:gameId/comments', isLoggedIn, gamesCtrl.createComment)
 
